c4bbdcfba24812a4b108a424e829abd19c661047
Kacper Pruszynski <kacper@telgani.com>
2025-04-07 09:40:58 +0200

    chore: remove cname

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
-
-
 export default defineConfig({
     root: 'src',
     plugins: [
